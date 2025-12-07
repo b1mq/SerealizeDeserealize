8544ff47accd67c2723b8d1eb9f5c9d3bdf1cd73
Egor <arnoldtagirov1@gmail.com>
2025-12-07 18:35:18 +0100

    -m

---
+++ Serealize/Domain/Entities/FakePerson.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace Serealize.Domain.Entities
+{
+    public class FakePerson
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        int Age { get; set; }
+        string Email { get; set; }
+        string Adress { get; set; }
+        private string _path = @"C:/Users/Admin/source/repos/b1mq/SerealizeDeserealize/Serealize/Domain/Entities/DataBlank/";
+        public FakePerson()
+        {
+            Name = string.Empty;
+            Surname = string.Empty;
+            Age = 0;
+            Email = string.Empty;
+            Adress = string.Empty;
+        }
+        public FakePerson(string name, string surname, int age, string email, string adress)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Email = email;
+            Adress = adress;
+        }
+        private  string GetPath() => _path; 
+        public  FakePerson GeneratePerson()
+        {
+            var Names = File.ReadAllText(GetPath() + "Names.json");
+            var Surnames = File.ReadAllText(GetPath() + "Surnames.json");
+            var Ages = File.ReadAllText(GetPath() + "Ages.json");
+            var Emails = File.ReadAllText(GetPath() + "Emails.json");
+            var Adresses = File.ReadAllText(GetPath() + "Adress.json");
+            var JsonNames = JsonSerializer.Deserialize<List<string>>(Names);
+            var JsonSurnames = JsonSerializer.Deserialize<List<string>>(Surnames);
+            var JsonAges = JsonSerializer.Deserialize<List<int>>(Ages);
+            var JsonEmails = JsonSerializer.Deserialize<List<string>>(Emails);
+            var JsonAdresses = JsonSerializer.Deserialize<List<string>>(Adresses);
+            Random rand = new Random();
+            return new FakePerson(JsonNames![rand.Next(0,JsonNames.Count)], JsonSurnames![rand.Next(0, JsonSurnames.Count)], JsonAges![rand.Next(0,JsonAges.Count)], JsonEmails![rand.Next(0, JsonEmails.Count)], JsonAdresses![rand.Next(0, JsonAdresses.Count)]);
+        }
+        public override string ToString()
+        {
+            return $"Name :{Name}\nSurname :{Surname}\nAge :{Age}\nAdress :{Adress}\nEmail :{Email}";
+        }
+    }
+}
--- Serealize/Program.cs
+++ Serealize/Program.cs
@@ -2,14 +2,18 @@
 
 using Serealize.Domain.Entities;
 
-var songA = new Song("Magnolia", "PlayBoy Carti", 2.52);
-var songB = new Song("Void", "Poloko", 3.51);
-List<Song> MySongs = new List<Song> { songA, songB };
-var AlbumA = new Album("MyAlbum", MySongs, 2025);
-var MyManager = new AlbumManagaer()
-{
-    Name = "Carti Collection",
-    Albums = new List<Album> { AlbumA }
-};
-//MyManager.SaveAlbumsInJson(MyManager,@"C:/Users/Admin/Desktop/Receipt", "Album");
-MyManager.LoadAlbumsFromJson(@"C:/Users/Admin/Desktop/Receipt", "Album");
+//var songA = new Song("Magnolia", "PlayBoy Carti", 2.52);
+//var songB = new Song("Void", "Poloko", 3.51);
+//List<Song> MySongs = new List<Song> { songA, songB };
+//var AlbumA = new Album("MyAlbum", MySongs, 2025);
+//var MyManager = new AlbumManagaer()
+//{
+//    Name = "Carti Collection",
+//    Albums = new List<Album> { AlbumA }
+//};
+////MyManager.SaveAlbumsInJson(MyManager,@"C:/Users/Admin/Desktop/Receipt", "Album");
+//MyManager.LoadAlbumsFromJson(@"C:/Users/Admin/Desktop/Receipt", "Album");
+
+var fp = new FakePerson();
+var s = fp.GeneratePerson();
+Console.WriteLine(s);
